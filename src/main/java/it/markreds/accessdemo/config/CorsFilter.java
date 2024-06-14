bcdd0f6fe03d87714bbfe28a61e9e1569cc4ca7f
Mark Reds <marco@markreds.it>
2024-06-14 08:19:15 +0200

    Added CorsFilter

---
+++ src/main/java/it/markreds/accessdemo/config/CorsFilter.java
@@ -0,0 +1,17 @@
+package it.markreds.accessdemo.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
